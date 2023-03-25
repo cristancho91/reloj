8d92281e4c502f0a4a1c1293197728fc7e5d200f
cristancho91 <64867797+cristancho91@users.noreply.github.com>
2023-03-25 00:46:26 -0500

    reloj version 1

---
+++ script.js
@@ -0,0 +1,22 @@
+const reloj = document.querySelector('#reloj');
+
+const mecanismo = ()=>{
+    let tiempo = new Date();
+    let horas = tiempo.getHours();
+    let minutos = tiempo.getMinutes();
+    let segundos = tiempo.getSeconds();
+
+    if(horas < 10 ){
+        horas = '0'+ horas;
+    }
+    if(minutos < 10){
+        minutos = '0'+ minutos;
+    }
+    if(segundos < 10){
+        segundos = '0'+segundos;
+    }
+
+    let cadenaTxt = horas + ' : '+minutos+' : '+ segundos;
+    reloj.textContent = cadenaTxt;
+}
+setInterval(mecanismo, 1000);
